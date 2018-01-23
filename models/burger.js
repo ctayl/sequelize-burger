4caba128e5aaba1d7a1f2c39f2ecb5a429c6c49d
chris <chrisb000@gmail.com>
2018-01-22 21:17:48 -0800

    sync

--- models/burger.js
+++ models/burger.js
@@ -1,73 +1,18 @@
-var orm = require("../config/orm");
-// var connection = require("../config/connection");
+module.exports = function (sequelize, DataTypes) {
 
-var burger = {
+    var Burger = sequelize.define("burger", {
+        burger_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validation: {
+                len: [1, 50]
+            }
+        },
+        devoured: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+    });
 
-    // gets all burgers
-    all: function (callback) {
-        orm.selectAll("burgers", function(res) {
-            callback(res);
-        });
-    },
-
-    // makes a new burger
-    new: function (name, callback) {
-        orm.insertInto("burgers", name, function(res) {
-            callback(res);
-        });
-    },
-
-    // changes the value of "devoured" to true
-    eat: function (id, callback) {
-        orm.update("burgers", id, "devoured", true, function (res) {
-            callback(res);
-        });
-    },
-
-    // changes the value of "devoured" to false
-    make: function (id, callback) {
-        orm.update("burgers", id, "devoured", false, function (res) {
-            callback(res);
-        });
-    },
-
-    // removes a burger from the database
-    remove: function (id, callback) {
-        orm.remove("burgers", id, function (res) {
-            callback(res);
-        });
-    }
-};
-
-module.exports = burger;
-
-
-
-
-
-
-// module.exports = function (app) {
-
-//     app.get("/", function(req, res) {
-//         res.sendFile(path.join(__dirname, "../public/index.html"))
-//     });
-
-//     app.get("/test", function(req, res) {
-//         res.render("test", {test: "test"})
-//     });
-
-//     app.get("/api/all", function(req, res) {
-//         orm.selectAll("burgers", function (data) {
-//             console.log(data);
-//             res.json(data);
-//         });
-//     });
-    
-//     app.post("/api/new", function (req, res) {
-//         console.log(req.body)
-//         orm.insertInto("burgers", req.body, function(data) {
-//             res.json(data);
-//         });
-//     });
-
-// }
+    return Burger;
+}
--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 var bp = require('body-parser');
 
 var express = require('express');
-var PORT =  process.env.PORT || 3300 ;
+var PORT = process.env.PORT || 3300;
 
 var app = express();
 
-app.use(bp.urlencoded({extended: true}));
+app.use(bp.urlencoded({
+    extended: true
+}));
 app.use(bp.json());
 
 app.use(express.static("public"));
+var db = require('./models');
 
 var routes = require('./controllers/controller');
 app.use(routes);
 
 var handlebars = require('express-handlebars');
-app.engine("handlebars", handlebars({ defaultLayout: "main"}));
+app.engine("handlebars", handlebars({
+    defaultLayout: "main"
+}));
 app.set("view engine", "handlebars");
 
-app.listen(PORT, function(){
-    console.log(`connected on port ${PORT}`)
+db.sequelize.sync().then(function () {
+    app.listen(PORT, function () {
+        console.log(`connected on port ${PORT}`)
+    });
 });
